1555e1d34d9e6f8bcfa4e7eccf508b137f0fc3bf
Patricia Tami Sakita <tamisakita@hotmail.com>
2022-09-28 20:17:50 -0300

    implementação de segurança

--- src/main/java/com/project/fiapnotes/FiapNotesApplication.java
+++ src/main/java/com/project/fiapnotes/FiapNotesApplication.java
@@ -2,12 +2,14 @@ package com.project.fiapnotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FiapNotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FiapNotesApplication.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("senha123"));
 	}
 
 }
---
+++ src/main/java/com/project/fiapnotes/models/UserModel.java
@@ -0,0 +1,71 @@
+package com.project.fiapnotes.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "TB_USER")
+public class UserModel implements UserDetails, Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany
+    @JoinTable(name = "TB_USERS_ROLES",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<RoleModel> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
